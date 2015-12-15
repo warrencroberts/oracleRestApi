e7c1fc8354efd2f69a57127dae7a07bdd20c8730
wroberts <warren.roberts@icd-tech.com>
2015-12-15 11:15:09 -0700

    All services except destroy all are working.

--- events/app/controllers/events_controller.rb
+++ events/app/controllers/events_controller.rb
@@ -2,59 +2,75 @@ class EventsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-   Rails.logger.debug "---------> index -- top"
-   @events = Event.all
-   Rails.logger.debug "---------> index -- found #{@events.size} events"
-   render :json => @events
-  end
-
-  def new
-    Rails.logger.debug "---------> new -- top"
-    @event = Event.new
-  end
-
-  def edit
-    Rails.logger.debug "---------> create -- top -- with params : #{params}"
-    @event = Event.find(params[:id])
+    Rails.logger.debug "---------> index -- top : #{params}"
+    theEvents = Event.all
+    Rails.logger.debug "---------> index -- found #{theEvents.size} events"
+    render :json => theEvents.collect {|event| event.data}
   end
 
   def create
     Rails.logger.debug "---------> create -- top -- with params : (#{params})"
-    Rails.logger.debug "---------> create print some json (#{params['event']['data']})"
 
-    @event = Event.create(data: params['event']['data'])
+    the_new_event = Event.create(data: params['event']['data'])
 
     respond_to do |format|
-      format.json {render :json => "OK"}
+      format.json {render :json => the_new_event.data}
     end
   end
 
   def update
     Rails.logger.debug "---------> update -- top -- with params : #{params}"
-    @event = Event.find(params[:id])
+    result,the_event = get_result_and_event(params)
+    Rails.logger.debug "---------> update -- result : #{result}"
 
-    if @event.update(event_params)
-      redirect_to @event
-    else
-      render 'edit'
+    the_event.data = params['event']['data']
+    the_event.save
+
+    respond_to do |format|
+      format.json {render :json => the_event.data}
     end
+
   end
 
   def show
-    Rails.logger.debug "---------> update -- top -- with params : #{params}"
-    @event = Event.find(params[:id])
+    Rails.logger.debug "---------> show -- top -- with params : #{params}"
+    result = get_result_and_event(params)
+    Rails.logger.debug "---------> show -- result : #{result}"
+
+    respond_to do |format|
+      format.json {render :json => result[0] }
+    end
   end
 
   def destroy
     Rails.logger.debug "---------> destroy -- top --with params : #{params}"
-    @event = Event.find(params[:id])
-    @event.destroy
+    result,the_event = get_result_and_event(params)
+    Rails.logger.debug "---------> destroy -- result : #{result}"
 
-    redirect_to events_path
+    if(the_event)
+      the_event.destroy
+    end
+
+    respond_to do |format|
+      format.json {render :json => result }
+    end
   end
 
   private
   def event_params
     params.require(:event).permit(:data)
   end
+
+  def get_result_and_event(theParams)
+    recordId = theParams[:id]
+    result = "MISSING"
+    event  = nil
+    if (Event.exists?(recordId))
+      event = Event.find(recordId)
+      result = event.data
+    end
+
+    return result, event
+  end
+
 end
