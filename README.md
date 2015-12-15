b672b9557ffcd1f3d4ae1112f0f5b8f15f6c8748
wroberts <warren.roberts@icd-tech.com>
2015-12-15 12:44:15 -0700

    Completed deleteall and tested all the services

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # oracleRestApi
 Repository for a rest api coding project given to me by Oracle
+
+This project was implemented with Ruby 2.2.1 and Rails 4.2.5
+
+The rails platform has very good support for implementing restful web services which is why I selected it.  By default rails runs on port 3000 although that can be changed on the command line.  It also uses an sqlite3 database that is located in oracleRestApi/events/db/development.sqlite3.
+
+To run it you need to install the required ruby and rails versions via rvm.  To run the code you first need to start up the rails server.  Here are the steps :
+
+  1.) cd to the oracleRestApi/events directory
+  2.) rails server --port 8999
+
+  After the rails server has started successfully you should be able to execute curl commands against it.  Note that I included the results of my test run in oracleRestApi/test_run_results.txt
+
+  Let me know if you have any trouble getting it running.
+
+  Warren
--- events/app/controllers/events_controller.rb
+++ events/app/controllers/events_controller.rb
@@ -5,7 +5,7 @@ class EventsController < ApplicationController
     Rails.logger.debug "---------> index -- top : #{params}"
     theEvents = Event.all
     Rails.logger.debug "---------> index -- found #{theEvents.size} events"
-    render :json => theEvents.collect {|event| event.data}
+    render :json => theEvents.collect {|event| [event.id,event.data]}
   end
 
   def create
@@ -14,45 +14,56 @@ class EventsController < ApplicationController
     the_new_event = Event.create(data: params['event']['data'])
 
     respond_to do |format|
-      format.json {render :json => the_new_event.data}
+      format.json {render :json => [the_new_event.id, the_new_event.data]}
     end
   end
 
   def update
     Rails.logger.debug "---------> update -- top -- with params : #{params}"
-    result,the_event = get_result_and_event(params)
-    Rails.logger.debug "---------> update -- result : #{result}"
+    the_event = get_event()
+    Rails.logger.debug "---------> update -- result : #{the_event}"
 
-    the_event.data = params['event']['data']
-    the_event.save
+    if(the_event)
+      the_event.data = params['event']['data']
+      the_event.save
+    end
 
     respond_to do |format|
-      format.json {render :json => the_event.data}
+      format.json {render :json =>  get_return_result(the_event)}
     end
 
   end
 
   def show
     Rails.logger.debug "---------> show -- top -- with params : #{params}"
-    result = get_result_and_event(params)
-    Rails.logger.debug "---------> show -- result : #{result}"
+    the_event = get_event()
+    Rails.logger.debug "---------> show -- the_event : [#{params[:id]}, #{the_event}]"
 
     respond_to do |format|
-      format.json {render :json => result[0] }
+      format.json {render :json => get_return_result(the_event)}
     end
   end
 
   def destroy
     Rails.logger.debug "---------> destroy -- top --with params : #{params}"
-    result,the_event = get_result_and_event(params)
-    Rails.logger.debug "---------> destroy -- result : #{result}"
+    the_event = get_event()
+    Rails.logger.debug "---------> destroy -- result : #{the_event}"
 
     if(the_event)
       the_event.destroy
     end
 
     respond_to do |format|
-      format.json {render :json => result }
+      format.json {render :json => [params[:id], nil] }
+    end
+  end
+
+  def destroy_all
+    Rails.logger.debug "---------> destroy_all -- top --with params : #{params}"
+    Event.destroy_all
+
+    respond_to do |format|
+      format.json {render :json => "OK" }
     end
   end
 
@@ -61,16 +72,18 @@ class EventsController < ApplicationController
     params.require(:event).permit(:data)
   end
 
-  def get_result_and_event(theParams)
-    recordId = theParams[:id]
-    result = "MISSING"
+  def get_event()
+    recordId = params[:id]
     event  = nil
     if (Event.exists?(recordId))
       event = Event.find(recordId)
-      result = event.data
     end
 
-    return result, event
+    return event
+  end
+
+  def get_return_result(the_event)
+    [params[:id], the_event ? the_event.data : nil]
   end
 
 end
